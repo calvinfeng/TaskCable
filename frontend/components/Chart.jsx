bceb325b98af5f8f6e5f4b6c556c45ca68f95ead
Calvin Feng <calvinjhfeng@gmail.com>
2017-09-18 23:35:39 -0700

    Finalized my choice for pie chart

--- frontend/components/Chart.jsx
+++ frontend/components/Chart.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { PieChart, Pie } from 'recharts';
+import { PieChart, Pie, Tooltip, Cell } from 'recharts';
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const data01 = [
-  { name: 'Group A', value: 400, label: 'Group A' },
+  { name: 'Group A', value: 400 },
   { name: 'Group B', value: 300 },
   { name: 'Group C', value: 300 },
   { name: 'Group D', value: 200 }
 ];
 
 const data02 = [
+  { name: 'Group A', value: 200 },
+  { name: 'Group B', value: 500 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 }
+];
+
+const data03 = [
   { name: 'A1', value: 100 },
   { name: 'A2', value: 300 },
   { name: 'B1', value: 100 },
@@ -24,15 +33,24 @@ const data02 = [
 
 class Chart extends React.Component {
   state = {
-    data: data01
+    data: data01,
+    activeIndex: 0
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        data: data02
-      });
-    }, 2000);
+    setInterval(() => {
+      if (this.state.data === data01) {
+        this.setState({ data: data02 });
+      } else {
+        this.setState({ data: data01 });
+      }
+    }, 3000);
+  }
+
+  handleMouseEnter = (data, index) => {
+    this.setState({
+      activeIndex: index
+    });
   }
 
   /**
@@ -41,11 +59,16 @@ class Chart extends React.Component {
    */
   render() {
     return (
-      <PieChart width={800} height={400}>
-        <Pie data={data01} cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
-        <Pie data={data02} cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" />
-        <Pie data={this.state.data} cx="50%" cy="50%" innerRadius={90} outerRadius={120} fill="#8884d8" label />
-      </PieChart>
+      <div>
+        <PieChart width={$('#react-application').width()} height={$('#react-application').height()}>
+          <Pie data={data01} cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+          <Pie data={data03} cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" />
+          <Pie data={this.state.data} cx="50%" cy="50%" innerRadius={90} outerRadius={120} fill="#8884d8" label>
+            {this.state.data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)}
+          </Pie>
+          <Tooltip />
+        </PieChart>
+      </div>
     );
   }
 }
